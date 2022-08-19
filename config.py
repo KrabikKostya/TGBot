3443873ff433471aaeb0db632fb04e1b8f6b1c88
KrabikKostya <kostya.krabik.sw@gmail.com>
2022-08-19 23:50:09 +0300

    meybe fix some problem 2

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-from dotenv import get_variables
+import dotenv
 
-
-token = get_variables(".env")["bot_token"]
-group_id = int(get_variables(".env")["group_id"])
+token = dotenv.get_variables(".env")["bot_token"]
+group_id = int(dotenv.get_variables(".env")["group_id"])
