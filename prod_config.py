320176cd997cd0b381597ac72022103310c03697
KrabikKostya <kostya.krabik.sw@gmail.com>
2022-08-22 19:01:46 +0300

    fix a heroku cheking

--- prod_config.py
+++ prod_config.py
@@ -2,10 +2,7 @@ import os
 
 
 def prod_configurator():
-    is_heroku = os.environ.get('IS_HEROKU', None)
-    print(is_heroku)
-    if is_heroku:
-        import config
+    import config
 
-        config.bot_owner = int(os.environ.get('bot_owner', None))
-        config.bot_token = os.environ.get('bot_token', None)
+    config.bot_owner = int(os.environ.get('bot_owner', None))
+    config.bot_token = os.environ.get('bot_token', None)
