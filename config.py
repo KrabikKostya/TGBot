9c8d80d6da8689d20df843e6562192eb445278ec
KrabikKostya <kostya.krabik.sw@gmail.com>
2022-08-22 19:06:34 +0300

    fix some mistake 2

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
 
-bot_owner = int(os.environ.get('bot_owner', None))
-bot_token = os.environ.get('bot_token', None)
+bot_owner = int(os.environ.get('bot_owner'))
+bot_token = os.environ.get('bot_token')
