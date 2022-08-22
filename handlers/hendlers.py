a34b835e1f7510757b2c1811bc9ace8bc1222347
KrabikKostya <kostya.krabik.sw@gmail.com>
2022-08-22 20:32:09 +0300

    test a way to fix problem 4

--- handlers/hendlers.py
+++ handlers/hendlers.py
@@ -61,17 +61,17 @@ async def cmd_mute(msg: Message):
     mute_time = 1
     try:
         if msg["text"].split()[2] == "h":
-            mute_time = float(msg.split()[1]) * 3600
+            mute_time = float(msg["text"].split()[1]) * 3600
         elif msg["text"].split()[2] == "d":
-            mute_time = float(msg.split()[1]) * 3600 * 24
+            mute_time = float(msg["text"].split()[1]) * 3600 * 24
         elif msg["text"].split()[2] == "w":
-            mute_time = float(msg.split()[1]) * 3600 * 24 * 7
+            mute_time = float(msg["text"].split()[1]) * 3600 * 24 * 7
         elif msg["text"].split()[2] == "m":
-            mute_time = float(msg.split()[1]) * 3600 * 24 * 30
+            mute_time = float(msg["text"].split()[1]) * 3600 * 24 * 30
         elif msg["text"].split()[2] == "y":
-            mute_time = float(msg.split()[1]) * 3600 * 24 * 365
+            mute_time = float(msg["text"].split()[1]) * 3600 * 24 * 365
     except IndexError:
-        mute_time = float(msg.msg["text"]()[1]) * 60
+        mute_time = float(msg["text"]()[1]) * 60
     await msg.bot.delete_message(config.group_id, msg.message_id)
     await msg.bot.restrict_chat_member(config.group_id, msg.reply_to_message.from_user.id, until_date=time.time() + mute_time)
     await msg.reply_to_message.reply("Посиди і подумай над своєю поведінкою")
