415549b59b95f8945054c41c2606c2e8764a246d
KrabikKostya <kostya.krabik.sw@gmail.com>
2022-08-22 18:59:12 +0300

    fix litle mistake

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import prod_config
 
 
+prod_config.prod_configurator()
 bot_owner = 0
 bot_token = ""
--- prod_config.py
+++ prod_config.py
@@ -1,9 +1,11 @@
 import os
 
 
-is_heroku = os.environ.get('IS_HEROKU', None)
-if is_heroku:
-    import config
+def prod_configurator():
+    is_heroku = os.environ.get('IS_HEROKU', None)
+    print(is_heroku)
+    if is_heroku:
+        import config
 
-    config.bot_owner = int(os.environ.get('bot_owner', None))
-    config.bot_token = os.environ.get('bot_token', None)
+        config.bot_owner = int(os.environ.get('bot_owner', None))
+        config.bot_token = os.environ.get('bot_token', None)
