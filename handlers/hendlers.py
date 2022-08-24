050b98c94fa64b3716a3460d3d73612cc3e3c8c7
KrabikKostya <kostya.krabik.sw@gmail.com>
2022-08-24 16:14:27 +0300

    fix all comands

--- handlers/hendlers.py
+++ handlers/hendlers.py
@@ -154,8 +154,8 @@ async def cmd_add(msg: Message):
 async def cmd_all(msg: Message):
     session = Session(bind=engine)
     users = session.query(Users).all()
-    print(users)
+    print(list(users))
     text = ""
     for i in range(len(list(users))):
-        text += str(cryptocode.decrypt(list(users)[i].tg_username, list(users)[i].kay_name))
+        text += "@" + str(cryptocode.decrypt(list(users)[i].tg_username, list(users)[i].kay_name)) + ""
     await msg.reply(text)
